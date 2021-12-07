6e70ef8f30f1477efbce17424b00df291db47af1
wdmuthiora <wdmuthiora@gmail.com>
2021-12-07 17:49:42 +0300

    bound the activity_details.xml's elements.

--- app/src/main/java/com/moringaschool/android_ip_1/UI/DetailsActivity.java
+++ app/src/main/java/com/moringaschool/android_ip_1/UI/DetailsActivity.java
@@ -3,14 +3,28 @@ package com.moringaschool.android_ip_1.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringaschool.android_ip_1.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    @BindView (R.id.tvMovieName) TextView tvMovieName;
+    @BindView (R.id.tvMovieReleaseYear) TextView tvMovieReleaseYear;
+    @BindView (R.id.tvMovieRating) TextView tvMovieRating;
+    @BindView (R.id.tvMovieDescription) TextView tvMovieDescription;
+    @BindView(R.id.ivMoviePoster) ImageView ivMoviePoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        ButterKnife.bind(this);
     }
 }
