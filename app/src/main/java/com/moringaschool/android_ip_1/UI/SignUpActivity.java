7eb7288a4ac99749b1496fcdd3bc32b1ac120373
wdmuthiora <wdmuthiora@gmail.com>
2021-12-14 20:10:21 +0300

    Configured Firebase's account creation via the SignUp Activity

--- app/src/main/java/com/moringaschool/android_ip_1/UI/SignUpActivity.java
+++ app/src/main/java/com/moringaschool/android_ip_1/UI/SignUpActivity.java
@@ -5,15 +5,22 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.android_ip_1.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    private FirebaseAuth firebaseAuth;
 
     @BindView(R.id.tvSignUpName) TextView tvSignUpName;
     @BindView(R.id.tvSignUpEmail) TextView tvSignUpEmail;
@@ -30,26 +37,52 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
 
         ButterKnife.bind(this);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
     }
 
     @Override
     public void onClick(View view) {
 
         if (view ==clSignUpCreateAccount){
+
             createNewUser();
+
         }
 
         if (view==tvSignUpSignIn){
+
             Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); /*FLAG_ACTIVITY_CLEAR_TASK will cause any existing task that would be associated with the activity to be cleared before the activity is started. This prevents this Activity from being unnecessarily accessed via the system back button. FLAG_ACTIVITY_NEW_TASK will make the activity we are navigating to the start of a brand new task on this history stack.*/
             startActivity(intent);
             finish();
-        }
 
+        }
 
     }
 
     private void createNewUser() {
+
+        //capture input from user.
+        final String name = tvSignUpName.getText().toString().trim();
+        final String email = tvSignUpEmail.getText().toString().trim();
+        String password = tvSignUpPassword.getText().toString().trim();
+        String confirmPassword = tvSignUpConfirmPassword.getText().toString().trim();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, task -> {
+
+            if (task.isSuccessful()){
+
+                Log.d(TAG, "Firebase Authentication is successful.");
+
+            }else {
+
+                Toast.makeText(SignUpActivity.this, "Firebase Authentication has failed.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
 }
